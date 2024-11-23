0aa6f73689fabb1a53ded36eb53e8632412bdc1b
Gaby <133404217+Gabr1ell1@users.noreply.github.com>
2024-11-23 13:46:32 -0300

    Update PessoaDao.kt

--- Kotlin_BD/app/src/main/java/com/example/myapplication/roomDB/PessoaDao.kt
+++ Kotlin_BD/app/src/main/java/com/example/myapplication/roomDB/PessoaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import com.example.myapplication.roomDB
 import kotlinx.coroutines.flow.Flow
 
 @Dao
