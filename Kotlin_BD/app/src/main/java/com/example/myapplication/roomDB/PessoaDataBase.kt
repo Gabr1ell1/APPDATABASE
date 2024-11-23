979e8b473d12367203eb25984cb3c32e1b249e83
Gaby <133404217+Gabr1ell1@users.noreply.github.com>
2024-11-23 13:53:31 -0300

    Update PessoaDataBase.kt

--- Kotlin_BD/app/src/main/java/com/example/myapplication/roomDB/PessoaDataBase.kt
+++ Kotlin_BD/app/src/main/java/com/example/myapplication/roomDB/PessoaDataBase.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.roomDB
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-
+import com.example.myapplication.roomDB
 
 @Database(
     entities = [Pessoa::class],
